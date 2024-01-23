8bc2fc8d023c3e601d259f8309529927834f2dd0
kagisho96 <kagishopitsi@gmail.com>
2024-01-23 22:26:19 +0200

    feat: query users name

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
-const UsersPage = () => {
+
+interface Users {
+    id: string
+    name: string
+}
+
+const UsersPage = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users")
+    const users: Users[] = await res.json()
   return (
     <div>
-      <p>UsersPage</p>
+      <h1>Users</h1>
+      <ul>
+        {users.map(user => <li key={user.id}>{user.name}</li> )}
+      </ul>
     </div>
   )
 }
